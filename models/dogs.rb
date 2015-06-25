1fd3d1ffe63575e3030a8296da44760bbb75ebea
Mark Cheng <mcheng161@gmail.com>
2015-06-25 23:34:49 +0000

    new change

--- models/dogs.rb
+++ models/dogs.rb
@@ -3,13 +3,50 @@ def get_results(params)
   small = 0
   medium = 0
   large = 0
-  array.each do |size|
-    if size == "small"
+  array.each do |place|
+    if place == "Small"
       small += 1 
-    elsif size == "medium"
+    elsif place == "Medium"
       medium += 1
     else
       large += 1
+    end
+  end
+   array.each do |home|
+    if home == "Small"
+      small += 1 
+    elsif home == "Medium"
+      medium += 1
+    else
+      large += 1
+      small += 1 
+      medium += 1
+    end
+  end
+  array.each do |size|
+    if size == "Small"
+      small += 1 
+    elsif size == "Medium"
+      medium += 1
+    else
+      large += 1
+    end
+  end
+  array.each do |personality|
+    if personality == "Small"
+      small += 1 
+    else
+      large += 1
+      medium += 1
+    end
+  end
+  array.each do |activity|
+    if activity == "Small, Medium"
+      small += 1
+      medium += 1
+    else
+      large += 1
+      medium += 1
     end
   end
     if small > medium && small > large 
