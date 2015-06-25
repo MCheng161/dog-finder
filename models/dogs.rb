36e94d3b105b3206fbd6be0be1bffff7ba98c600
Mark Cheng <mcheng161@gmail.com>
2015-06-25 18:18:28 +0000

    new change

--- models/dogs.rb
+++ models/dogs.rb
@@ -1,8 +1,26 @@
 
 def get_results(params)
-
-  
- 
+  array = params.values
+  small = 0
+  medium = 0
+  large = 0
+  array.each do |size|
+    if size == "small"
+      small += 1 
+    elsif size == "medium"
+      medium += 1
+    else
+      large += 1
+    end
+  end
+    if small > medium && small > large 
+      return "small"
+      elsif medium > small && medium > large
+      return "medium"
+    else
+      return "large"
+        
+  end
 end
 
   
